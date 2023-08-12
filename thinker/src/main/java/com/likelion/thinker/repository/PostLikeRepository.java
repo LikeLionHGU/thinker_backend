55c33f43957e1c59c287737cbc069fba85e7b7cc
김동규 <kdg1941@naver.com>
2023-08-13 03:34:53 +0900

    fix the postLikeRepository query error

--- thinker/src/main/java/com/likelion/thinker/repository/PostLikeRepository.java
+++ thinker/src/main/java/com/likelion/thinker/repository/PostLikeRepository.java
@@ -3,8 +3,9 @@ package com.likelion.thinker.repository;
 import com.likelion.thinker.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     @Query("select r from PostLike r where r.member.memberId = :memberId And r.post.postId = :postId")
-    PostLike findPostLikeByMemberIdAndPostId(Long memberId, Long postId);
+    PostLike findPostLikeByMemberIdAndPostId(@Param("memberId") Long memberId, @Param("postId") Long postId);
 }
